79bae69d56f1a415b731557b0c390a74e3ddbb43
rudra raval <ravalrudrap@gmail.com>
2025-05-10 12:47:25 -0400

    Deploy1

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
+  experimental: {
+    optimizeCss: true,
+  },
 }
 
 module.exports = nextConfig 
