c893e9ebb260c3d1e3e4392f1641751d780b5249
rudra raval <ravalrudrap@gmail.com>
2025-05-10 15:24:28 -0400

    changes1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  output: 'standalone',
   images: {
     unoptimized: true,
   },
